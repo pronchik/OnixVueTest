ae1275d3ad04e987d8d299e81b6cd03c16ae0fd9
pronchik <pronmaxim2001@gmail.com>
2021-11-29 19:07:07 +0200

    lesson 8

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -2,24 +2,25 @@
 .tasks
   h2
       span Tasks
-        .add-form
-          input(v-model="task_name" class='input-name' placeholder="Name:")
-          input(v-model="task_description" class='input-name' placeholder="Description:")
-          input(v-model="task_deadline" class='input-name' placeholder="Deadline:")
-          button(class='add-task' @click="submitForm(task_name,task_description,task_deadline)") +
-        .nadesti
-          .nades
-            span Name
-            span Description
-          span Deadline
-        .task(v-for='(task, index) in tasks' :key='task')
-          .name
-            | {{task.name}}
-          .description
-            | {{task.description1}}
-          .time
-            | {{task.time}}
-          button(class='delete-task' @click="deleteCart(index)") -
+      .add-form
+        input(v-model="task_name" class='input-name' placeholder="Name:")
+        input(v-model="task_description" class='input-name' placeholder="Description:")
+        input(v-model="task_deadline" class='input-name' placeholder="Deadline:")
+        button(class='add-task' @click="submitForm(task_name,task_description,task_deadline)") +
+      .nadesti
+        .nades
+          span Name
+          span Description
+        span Deadline
+      //transition-group(name='tasks' mode='out-in')
+      .task(v-for='(task, index) in tasks' :key='task.index' :ref="`task${index}`" class="list-item")
+        .name
+          | {{task.name}}
+        .description
+          | {{task.description1}}
+        .time
+          | {{task.time}}
+        button(class='delete-task' @click="deleteCart(index)") -
 </template>
 
 <script lang="ts">
@@ -56,6 +57,19 @@ export default defineComponent({
     task_description: { required }
   },
   methods: {
+    setDelay (currentIndex) {
+      setTimeout(() => {
+        Object.values(this.$refs as unknown as HTMLElement)[currentIndex].classList.add('increase')
+      }, 2000 * currentIndex)
+    },
+    blink () {
+      for (let i = 0; i < Object.values(this.$refs).length; i++) {
+        this.setDelay(i)
+        setTimeout(() => {
+          Object.values(this.$refs as unknown as HTMLElement)[i].classList.remove('increase')
+        }, 2000 * Object.values(this.$refs).length)
+      }
+    },
     deleteCart (task) {
       this.tasks.splice(task, 1)
       emitter.emit('changeNumber', this.tasks.length)
@@ -72,12 +86,19 @@ export default defineComponent({
         this.task_deadline = ''
         this.task_description = ''
         emitter.emit('changeNumber', this.tasks.length)
+        this.$nextTick(() => {
+          Object.values(this.$refs as unknown as HTMLElement)[Object.values(this.$refs).length - 1].classList.add('blink')
+          setTimeout(() => {
+            Object.values(this.$refs as unknown as HTMLElement)[Object.values(this.$refs).length - 1].classList.remove('blink')
+          }, 4000)
+        })
       } else {
         alert('Not submited')
       }
     }
   },
   mounted () {
+    this.blink()
     emitter.on('changeArr', () => {
       this.tasks = this.tasks.splice(1)
     })
